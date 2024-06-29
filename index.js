99ccb4617d1ebe0c215b545fed9c61bd6d1f7242
rosekamau <kamaurose236@gmail.com>
2024-06-29 23:21:08 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberofFeet){
   // Write your code here!
+  if(numberofFeet <= 400){
+    return "This one is on me!"
+  }
+  else if(numberofFeet > 2000 && numberofFeet < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(numberofFeet > 400 && numberofFeet < 2000){
+    return "That will be twenty bucks.";
+  }
+  else (numberofFeet >2500)
+    return "No can do.";
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if(city ==="NYC"){
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go."
+  }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much.";
+  }else if (tip === "not as generous"){
+    return "Thank you.";
+  }else{
+    return "Bye.";
+  }
 }
